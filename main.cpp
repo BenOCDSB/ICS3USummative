28aa755faff32eaf1415148890dfdd6ed1ee2a7e
BenOCDSB <bbota1@ocdsb.ca>
2025-12-04 12:54:48 -0500

    add image

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Branch Ben_0.1
 /*allegro wiki
 https://github.com/liballeg/allegro_wiki/wiki/Allegro-Vivace*/
 
@@ -5,35 +6,94 @@ https://github.com/liballeg/allegro_wiki/wiki/Allegro-Vivace*/
 
 int main()
 {
-    al_init();
-    al_install_keyboard();
+    if(!al_init())
+    {
+        printf("couldn't initialize allegro\n");
+        return 1;
+    }
+
+    if(!al_install_keyboard())
+    {
+        printf("couldn't initialize keyboard\n");
+        return 1;
+    }
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0);
+    if(!timer)
+    {
+        printf("couldn't initialize timer\n");
+        return 1;
+    }
+
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(1080, 720);
+    if(!queue)
+    {
+        printf("couldn't initialize queue\n");
+        return 1;
+    }
+
+    ALLEGRO_DISPLAY* disp = al_create_display(640, 480);
+    if(!disp)
+    {
+        printf("couldn't initialize display\n");
+        return 1;
+    }
+
     ALLEGRO_FONT* font = al_create_builtin_font();
+    if(!font)
+    {
+        printf("couldn't initialize font\n");
+        return 1;
+    }
 
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
+    bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
+
+    if(!al_init_image_addon())
+    {
+    printf("couldn't initialize image addon\n");
+    return 1;
+    }
+
+    ALLEGRO_BITMAP* mysha = al_load_bitmap("mysha.png");
+    if (!mysha)
+    {
+        printf("Couldn't load mysha\n");
+        return 1;
+    }
 
     al_start_timer(timer);
     while(1)
     {
         al_wait_for_event(queue, &event);
+        
+        switch(event.type)
+        {
+            case ALLEGRO_EVENT_TIMER:
+                // game logic goes here.
+                redraw = true;
+                break;
 
-        if(event.type == ALLEGRO_EVENT_TIMER)
-            redraw = true;
-        else if((event.type == ALLEGRO_EVENT_KEY_DOWN) || (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE))
+            case ALLEGRO_EVENT_KEY_DOWN:
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                break;
+        }
+
+        if(done)
             break;
 
         if(redraw && al_is_event_queue_empty(queue))
         {
             al_clear_to_color(al_map_rgb(120, 200, 50));
-            al_draw_text(font, al_map_rgb(100, 0, 255), 000, 0, 0, "DU DU DU DU Max Verstappen!");
+            al_draw_text(font, al_map_rgb(100, 0, 255), 0, 0, 0, "DU DU DU DU Max Verstappen!");
+            al_draw_bitmap(mysha, 0, 0, 0);
+            
             al_flip_display();
 
             redraw = false;
@@ -44,9 +104,7 @@ int main()
     al_destroy_display(disp);
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
+    al_destroy_bitmap(mysha);
 
     return 0;
 }
-
-
-//Branch Ben_0.1
