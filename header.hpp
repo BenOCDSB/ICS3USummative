7ff7b2529d646645be92753941c0130c44ac3135
BenOCDSB <bbota1@ocdsb.ca>
2025-12-10 12:48:12 -0500

    merge alavi's branch

--- header.hpp
+++ header.hpp
@@ -1,9 +1,81 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+
+#ifdef __APPLE__
+const int SCREEN_W = 1080 * 2;
+const int SCREEN_H = 720 * 2;
+#else
+const int SCREEN_W = 1080;
+const int SCREEN_H = 720;
+#endif
+
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+
+struct Cloud{
+    int cloud_width;
+    int cloud_height;
+    int posx;
+    int posy;
+    int speed;
+};
+
+struct CollisionObject{
+    ALLEGRO_BITMAP* image;
+    int PosX;
+    int PosY;
+};
+
+const int SPEED = 10;
+const int FPS = 60;
+
+float SCALE_SCREEN = 1;
+
+int MAP_WIDTH = SCREEN_W * 3;
+int MAP_HEIGHT = SCREEN_H * 3;
+
+const int DEADZONE_FACTOR = 200;
+
+const int DEADZONE_LEFT  = DEADZONE_FACTOR / SCALE_SCREEN;
+const int DEADZONE_RIGHT = (SCREEN_W - DEADZONE_FACTOR)  / SCALE_SCREEN;
+const int DEADZONE_TOP = DEADZONE_FACTOR / SCALE_SCREEN;
+const int DEADZONE_BOTTOM = (SCREEN_H - DEADZONE_FACTOR) / SCALE_SCREEN;
+
+enum {UP,DOWN,LEFT,RIGHT};
+
+#define color1 al_map_rgb(221,200,7)
+#define color2 al_map_rgb(150,200,10)
+
+const int FRAME_DELAY_MAX = 1;
+const int TERRAIN_FRAME_DELAY_MAX = 5;
+
+const int CLOUD_DELAY_FRAME = 10;
+const int MAX_CLOUD_COUNT = 4;
+const int CLOUD_SPEED = 2;
+
+const int BLIZZARD_DELAY = 5;
+
+const float pSpriteX = 240;
+const float pSpriteY = 240;
+
+void resetTerrain(int* tileIndexList, int requiredTiles, int numOfTiles);
+void createCloud(Cloud* cloudList, int* ind, int camX);
+void destroyCloud(Cloud* cloudList, int* ind, int camX);
+
+void createCollisionObject(CollisionObject* objList, int* currentIndex, ALLEGRO_BITMAP* image, int posX, int posY);
+
+
+bool collisionCheck(int px, int py, int ox, int oy, int pwidth, int pheight, int owidth, int oheight);
+bool collisionCheckRadius(int px, int py, int ox, int oy, int pradius, int oradius);
+void cameraUpdate(float* cameraPosition, int px, int py, int pWidth, int pHeight);
+
+void UpdateTimerString(char* timerString, int timeRemaining);
--- main.cpp
+++ main.cpp
@@ -1,123 +1,443 @@
-//Branch Ben_0.1
-/*allegro wiki
-https://github.com/liballeg/allegro_wiki/wiki/Allegro-Vivace*/
-
 #include "header.hpp"
 
-#define SLATEGREY   al_map_rgb(112,128,144)
 
-const float FPS = 60;
-const int SCREEN_W = 640;
-const int SCREEN_H = 480;
-
-int main(int argc, char *argv[])
-{
-    ALLEGRO_DISPLAY *display = nullptr;
-	ALLEGRO_EVENT_QUEUE *event_queue = nullptr;
-    ALLEGRO_TIMER *timer = nullptr;
-
-    //initialize allegro
+int main(int argc, char* argv[]){
     al_init();
+        srand(time(0));
 
-    //initialize display
-    display = al_create_display(SCREEN_W, SCREEN_H);
-	if (!display) {
-    	al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
-                                 nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-       	return -1;
-	}
 
-    //initialize keyboard routines
-	if (!al_install_keyboard()) {
-	    al_show_native_message_box(display, "Error", "Error", "failed to initialize the keyboard!",
-                                 nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-      	return -1;
-   	}
+    ALLEGRO_DISPLAY* display;
+    display = al_create_display(SCREEN_W,SCREEN_H);
 
-    //add image processor
- 	if (!al_init_image_addon()) {
-    	al_show_native_message_box(display, "Error", "Error",
-    		"Failed to initialize al_init_image_addon!",nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-    	return 0;
-	}
-
-    //create timer
-    timer = al_create_timer(1.0 / FPS);
-   	if (!timer) {
-   		al_show_native_message_box(display, "Error", "Error", "Failed to create timer!",
-                                 nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-        return -1;
+    if(!display){
+        printf("display could not be created");
+        return 1;
     }
 
-    // set up event queue
-	event_queue = al_create_event_queue();
-	if (!event_queue) {
-		al_show_native_message_box(display, "Error", "Error", "Failed to create event_queue!",
-                                 nullptr, ALLEGRO_MESSAGEBOX_ERROR);
-		al_destroy_display(display);
-      	return -1;
-	}
+    // // same size on all device...
+    // int fb_w = al_get_display_width(display);
+    // int fb_h = al_get_display_height(display);
 
-    //register events for event queue
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    // float sx = (float)fb_w / SCREEN_W;
+    // float sy = (float)fb_h / SCREEN_H;
 
-    ALLEGRO_BITMAP* mysha = nullptr;
-    mysha = al_load_bitmap("mysha.png");
+    // ALLEGRO_TRANSFORM t;
+    // al_identity_transform(&t);
+    // al_scale_transform(&t, sx, sy);
+    // al_use_transform(&t);
+    // // ...
 
-    int dx = 0;
-	int dy = 0;
-    int posx = 100;
-	int posy = 100;
-	al_clear_to_color(SLATEGREY);
-	al_draw_bitmap(mysha, posx, posy, 0);
-	al_flip_display();
-	bool doexit = false;
 
-    al_start_timer(timer);
+    if (!(al_init_primitives_addon() &&
+    al_install_keyboard() &&
+    al_install_mouse() &&
+    al_init_image_addon() &&
+    al_init_font_addon() &&
+    al_init_ttf_addon())){
+        printf("Failed initialization");
+        return 1;
+    };
+
+
+    ALLEGRO_FONT* timerCountFont = al_load_font("assets/fonts/Anta-Regular.ttf", 50, 0);
     
-	while (!doexit) {
+    if(!timerCountFont){
+        printf("failed to load font");
+        return 1;
+    }
 
-    	ALLEGRO_EVENT ev;
-      	al_wait_for_event(event_queue, &ev);
 
-      	if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-        	doexit = true;
-      	}
-      	else if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
-         	switch(ev.keyboard.keycode) {
-            	case ALLEGRO_KEY_UP:
-               		dy -= 8;
-               		break;
-	            case ALLEGRO_KEY_DOWN:
-    		        dy += 8;
-            		break;
-            	case ALLEGRO_KEY_LEFT:
-               		dx -= 8;
-               		break;
-	            case ALLEGRO_KEY_RIGHT:
-               		dx += 8;
-               		break;
-               	case ALLEGRO_KEY_ESCAPE:
-               		doexit = true;
-               		break;
-         	}
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);
+    ALLEGRO_TIMER* blizzard_timer = al_create_timer(BLIZZARD_DELAY);
+    ALLEGRO_TIMER* secondTick = al_create_timer(1);
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_timer_event_source(blizzard_timer));
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+    ALLEGRO_KEYBOARD_STATE keystate;
+
+
+    ALLEGRO_BITMAP* snowTiles[6] = {al_load_bitmap("assets/images/snowTiles/Snow_0.png"),
+                                    al_load_bitmap("assets/images/snowTiles/Snow_1.png"),
+                                    al_load_bitmap("assets/images/snowTiles/Snow_2.png"),
+                                    al_load_bitmap("assets/images/snowTiles/Snow_3.png"),
+                                    al_load_bitmap("assets/images/snowTiles/Snow_4.png"),
+                                    al_load_bitmap("assets/images/snowTiles/Snow_5.png"),
+                                    };
+
+    for(int i = 0; i < 6; i++){
+        if(!snowTiles[i]){
+            printf("tiles initialization failed");
         }
-            
-            else if (ev.type == ALLEGRO_EVENT_TIMER) {
-            posx += dx;
-            posy += dy;
+    }
 
-         	al_clear_to_color(SLATEGREY);
-		 	al_draw_bitmap(mysha, dx, dy, 0);
-		 	al_flip_display();
-		}
-	}
+    int numCols = MAP_WIDTH / al_get_bitmap_width(snowTiles[0]) + 5;
+    int numRows = MAP_HEIGHT / al_get_bitmap_height(snowTiles[0]) + 5;
+    int totalTiles = numCols * numRows;
+
+    int TimeRemaining = 100;
+    char TimerString[15];
+
+    float cameraPosition[2] = {0,0};
+
+    ALLEGRO_TRANSFORM camera;
+    float fadeRate = 0.0;
+
+    int* tileIndexes = (int*)malloc(sizeof(int)*totalTiles);
+    resetTerrain(tileIndexes,totalTiles,sizeof(snowTiles)/sizeof(snowTiles[0]));
+   
+    int currentCloudCount = 0;
+    int cloudFrameCount = 0;
+    Cloud* cloudList = (Cloud*)malloc(sizeof(Cloud)*MAX_CLOUD_COUNT);
+
+    ALLEGRO_BITMAP* playerDown = al_load_bitmap("assets/images/Assassin/Spritesheets/Front - Walking.png");
+    ALLEGRO_BITMAP* playerUp = al_load_bitmap("assets/images/Assassin/Spritesheets/Back - Walking.png");
+    ALLEGRO_BITMAP* playerLeft = al_load_bitmap("assets/images/Assassin/Spritesheets/Left - Walking.png");
+    ALLEGRO_BITMAP* playerRight = al_load_bitmap("assets/images/Assassin/Spritesheets/Right - Walking.png");
+    ALLEGRO_BITMAP* player;
+
+    if(!(playerDown && playerUp && playerLeft && playerRight)){
+        printf("player images initialization failded");
+        return 1;
+    }
+
+    bool run = true;
+    bool draw = true;
+
+
+    bool active = false;
+    int aniX = 0;
+    int aniY = 0;
+    float frameDelay = 0;
+    int delayTerrainChange = TERRAIN_FRAME_DELAY_MAX;
+
+    if(!player){
+        printf("plauer image error");
+        return 1;
+    }
+
+    float x = 20;
+    float y = 20;
+
+
+    int px = 0;
+    int qx = SCREEN_W;
+    int direction = DOWN;
+    ALLEGRO_COLOR ccc = al_map_rgb(213,5,200);
+
+    int currentCollObjCount = 0;
+    CollisionObject* collObjs = (CollisionObject*)malloc(sizeof(CollisionObject) * 10);
+
+    createCollisionObject(collObjs, &currentCollObjCount, playerDown, 300, 300);
+    createCollisionObject(collObjs, &currentCollObjCount, playerLeft, 2800, 400);
+
+    // al_hide_mouse_cursor(display);
+    player = playerUp;
+    
+    double last_time = al_get_time();
+
+    al_start_timer(blizzard_timer);
+    al_start_timer(timer);
+    while (run){
+        ALLEGRO_EVENT events;
+        al_wait_for_event(event_queue,&events);
+
+        if(events.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+            run = false;
+            break;
+        }
+
+        // else if(events.type == ALLEGRO_EVENT_MOUSE_AXES){
+        //     x = events.mouse.x;
+        //     y = events.mouse.y;
+        // }
+        else if(events.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){
+            if(events.mouse.button & 1){
+                ccc = color1;
+            }
+            if(events.mouse.button & 2){
+                ccc = color2;
+            }
+        }
+
+
+        else if(events.type == ALLEGRO_EVENT_TIMER){
+           if (events.timer.source == timer){
+                active = true;
+            delayTerrainChange++;
+            cloudFrameCount++;
+
+
+            al_get_keyboard_state(&keystate);
+            if(al_key_down(&keystate,ALLEGRO_KEY_UP)){
+                y -= SPEED;
+                direction = UP;
+                player = playerUp;
+            }
+            else if(al_key_down(&keystate,ALLEGRO_KEY_DOWN)){
+                y += SPEED;
+                direction = DOWN;
+                player = playerDown;
+            }
+            else if(al_key_down(&keystate,ALLEGRO_KEY_LEFT)){
+                x -= SPEED;
+                direction = LEFT;
+                player = playerLeft;
+            }
+            else if(al_key_down(&keystate,ALLEGRO_KEY_RIGHT)){
+                x += SPEED;
+                direction = RIGHT;
+                player = playerRight;
+            }
+            else{
+                active = false;
+                aniX = 0;
+                aniY = 0;
+            }
+
+
+            if(active){
+                frameDelay += 1;
+                if(frameDelay >= FRAME_DELAY_MAX){
+                    aniX = (aniX + 1);
+                    if(aniX >= 4){
+                        aniY = (aniY+1) % 5;
+                        aniX = 0;
+                    }
+                    frameDelay = 0;
+                }
+            }
+
+            cameraUpdate(cameraPosition,x,y,pSpriteX,pSpriteY);
+            al_identity_transform(&camera);
+            al_translate_transform(&camera,-cameraPosition[0],-cameraPosition[1]);
+            al_scale_transform(&camera,SCALE_SCREEN,SCALE_SCREEN);
+            al_use_transform(&camera);
+
+            for(int i = 0; i < currentCollObjCount; i++){
+                if(collisionCheck(x,y,collObjs[i].PosX,collObjs[i].PosY,pSpriteX,pSpriteY,pSpriteX,pSpriteY)){
+                    if(direction == UP){
+                        y += SPEED;
+                    }
+                    else if(direction == DOWN){
+                        y -= SPEED;
+                    }
+                    else if(direction == LEFT){
+                        x += SPEED;
+                    }
+                    else if(direction == RIGHT){
+                        x -= SPEED;
+                    }
+                }
+            }
+        
+
+
+            if(cloudFrameCount >= CLOUD_DELAY_FRAME){
+                if(currentCloudCount <= MAX_CLOUD_COUNT){
+                    createCloud(cloudList,&currentCloudCount,cameraPosition[0]);
+                }
+                cloudFrameCount = 0;
+            }
+
+
+            // if(delayTerrainChange >= TERRAIN_FRAME_DELAY_MAX){
+            //     resetTerrain(tileIndexes,totalTiles,sizeof(snowTiles)/sizeof(snowTiles[0]));
+            //     delayTerrainChange = 0;
+            // }
+
+
+            if(px < -SCREEN_W){
+                px = SCREEN_W;
+            }
+            if(qx < -SCREEN_W){
+                qx = SCREEN_W;
+            }
+            px -= 10;
+            qx -= 10;
+
+
+            draw = true;
+           }
+
+        //    if (events.timer.source == blizzard_timer) {
+        //         double now = al_get_time();
+        //         double dura = now - last_time;
+
+
+        //         printf("Blizzard!!!!!\n");
+        //         printf("took: %lf secs\n", dura);
+        //     }
+            
+        }
+
+
+        if(draw){
+            draw = false;
+            al_clear_to_color(al_map_rgb(0,0,0));
+
+            int i = 0;
+            for (int r = 0; r < numRows; r++) {
+                for (int c = 0; c < numCols; c++) {
+                        al_draw_bitmap(
+                            snowTiles[2], c * 32, r * 32, 0
+                        );
+                    i++;
+                }
+            }
+
+           
+            // al_draw_filled_rectangle(px,0,px+SCREEN_W,SCREEN_H,al_map_rgb(2,50,120));
+            // al_draw_filled_rectangle(qx,0,qx+SCREEN_W,SCREEN_H,al_map_rgb(120,50,2));
+            // al_draw_filled_rectangle(x,y,x+100,y+100,c);
+            al_draw_bitmap_region(player,aniX*pSpriteX,aniY*pSpriteY,pSpriteX,pSpriteY,x,y,0);
+
+            for(int i = 0; i < currentCollObjCount; i++){
+                CollisionObject cObj = collObjs[i];
+                al_draw_bitmap_region(cObj.image,0,0,pSpriteX,pSpriteY,cObj.PosX,cObj.PosY,0);
+            }
+            for(int i = 0; i < currentCloudCount; i++){
+                Cloud cCloud = cloudList[i];
+                al_draw_filled_ellipse(cCloud.posx,cCloud.posy,cCloud.cloud_width,cCloud.cloud_height,al_map_rgba(15, 15, 15, 70));
+                cloudList[i].posx -= cloudList[i].speed;
+            }
+            
+            UpdateTimerString(TimerString, TimeRemaining);
+            al_draw_text(timerCountFont,al_map_rgb(34,35,125),SCREEN_W/2 + cameraPosition[0], 100 + cameraPosition[1],ALLEGRO_ALIGN_CENTRE,TimerString);
+
+            destroyCloud(cloudList,&currentCloudCount,cameraPosition[0]);
+
+            al_flip_display();
+        }
+    }
 
     al_destroy_display(display);
+    al_destroy_timer(timer);
+    al_destroy_bitmap(player);
+    al_destroy_bitmap(playerDown);
+    al_destroy_bitmap(playerUp);
+    al_destroy_bitmap(playerLeft);
+    al_destroy_bitmap(playerRight);
+    al_destroy_font(timerCountFont);
+    for (int i = 0; i < 6; i++) {
+    if (snowTiles[i]) {
+        al_destroy_bitmap(snowTiles[i]);
+        }
+    }
+    free(tileIndexes);
+    free(cloudList);
+    free(collObjs);
+
     al_destroy_event_queue(event_queue);
-    al_destroy_bitmap(mysha);
+   
+
 
     return 0;
+}
+
+
+void resetTerrain(int* tileIndexList, int requiredTiles, int numOfTiles){
+
+    for (int i = 0; i < requiredTiles; i++) {
+    tileIndexList[i] = 2; // Example
+}
+
+}
+
+
+void createCloud(Cloud* cloudList, int* ind, int camX){
+
+
+    int w = rand() % (150 + 1 - 100) + 100;
+    int h = rand() % (100 + 1 - 60) + 60;
+    int y = rand() % (SCREEN_H-100 + 1 - 0) + 0;
+    int x = SCREEN_W + camX + rand() % (SCREEN_W  + 1 - SCREEN_W / 5) + SCREEN_W / 5;
+    int s = rand() % (5 + 1 - 3) + 3;
+
+
+    Cloud newCloud;
+    newCloud.cloud_width = w;
+    newCloud.cloud_height = h;
+    newCloud.posx = x;
+    newCloud.posy = y;
+    newCloud.speed = s;
+
+
+    cloudList[*ind] = newCloud;
+    (*ind)++;
+}
+
+void destroyCloud(Cloud* cloudList, int* ind, int camX)
+{
+    int place = 0;
+    int leftBound = camX - 200;  // allow some buffer
+
+    for (int i = 0; i < *ind; i++) {
+        Cloud cCloud = cloudList[i];
+
+        if (cCloud.posx > leftBound) {
+            cloudList[place] = cCloud;
+            place++;
+        }
+    }
+
+    *ind = place;
+}
+
+void createCollisionObject(CollisionObject* objList, int* currentIndex, ALLEGRO_BITMAP* image, int posX, int posY){
+    CollisionObject newObj;
+    newObj.image = image;
+    newObj.PosX = posX;
+    newObj.PosY = posY;
+
+    objList[*currentIndex] = newObj;
+    (*currentIndex)++;
+
+}
+
+
+bool collisionCheck(int px, int py, int ox, int oy, int pwidth, int pheight, int owidth, int oheight){
+    if(px > ox + owidth || px + pwidth < ox || py > oy + oheight || py + pheight < oy){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+
+
+bool collisionCheckRadius(int px, int py, int ox, int oy, int pradius, int oradius){
+    if(sqrt(pow(px - ox, 2) + pow(py - oy, 2)) < pradius + oradius){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void cameraUpdate(float* cameraPosition, int px, int py, int pWidth, int pHeight)
+{
+    int playerCenterX = px + pWidth / 2;
+    int playerCenterY = py + pHeight / 2;
+
+    // Horizontal deadzone
+    if (playerCenterX < cameraPosition[0] + DEADZONE_LEFT)
+        cameraPosition[0] = playerCenterX - DEADZONE_LEFT;
+
+    else if (playerCenterX > cameraPosition[0] + DEADZONE_RIGHT)
+        cameraPosition[0] = playerCenterX - DEADZONE_RIGHT;
+
+    // Vertical deadzone
+    if (playerCenterY < cameraPosition[1] + DEADZONE_TOP)
+        cameraPosition[1] = playerCenterY - DEADZONE_TOP;
+
+    else if (playerCenterY > cameraPosition[1] + DEADZONE_BOTTOM)
+        cameraPosition[1] = playerCenterY - DEADZONE_BOTTOM;
+}
+
+void UpdateTimerString(char* timerString, int timeRemaining){
+    snprintf(timerString,15,"ddd%d",timeRemaining);
 }
